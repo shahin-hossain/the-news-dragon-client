61b13af0793c870c7846560b475722b5712c92ce
shahin hossain <shahinhossain3@hotmail.com>
2023-11-12 19:22:49 +0600

    Create Layout, Header Components and moment js

--- src/__Steps__.js
+++ src/__Steps__.js
@@ -6,13 +6,17 @@
  * 2. main.jsx => import 'bootstrap/dist/css/bootstrap.min.css';
  * 
  * **************************
- * BIG PROJECT REQUIREMENT
+ * BIG PROJECT REQUIREMENT (Component setup)
  * **************************
  * -------------
  * Route 
  * -------------
  * 1. big project এ Route গুলো আলাদা একটা folder এ রাখে। এখানে main route থাকতে পারে আবার private route গুলোও থাকতে পারে ।
  * 
+ * -------------
+ * Layouts
+ * --------------
+ * 1. 1. Big project এ layouts গুলো আলাদা একটা folder/directory এ setup করা থাকবে।
  * ---------------
  * pages
  * ---------------
@@ -21,5 +25,15 @@
  * ------------------
  * Shared
  * -----------------
- * 1. Shared Folder টা হলো যেখানের components গুলো বিভিন্ন জায়গায় শেয়ার হবে। যেমন nav bar
+ * 0. shared folder  টা pages এর মধে থাকবে।
+ * 1. Shared Folder টা হলো যেখানের components গুলো বিভিন্ন জায়গায় শেয়ার হবে। যেমন nav bar, footer
+ * 2. LeftNav.jsx
+ * 3. RightNav.jsx
+ * 
+ * ----------------
+ * Dynamic Time
+ * 
+ * Library -
+ * Moment JS : Install > npm install moment
+ * ----------------
  */
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import logo from '../../../assets/logo.png'
+import moment from 'moment';
 
 const Header = () => {
     return (
         <div>
-            <h2>Header page</h2>
+            <div className='text-center'>
+                <img src={logo} alt="the dragon news logo" />
+                <p className='text-secondary'><small>Journalism Without Fear or Favour</small></p>
+                {/* 
+                1. install moment js
+                2. import 
+                3. display > format 
+                4.-> dddd -> Day full
+                  -> MMMM -> Month Full
+                  -> Do -> D = Day, o = (numbering like 1st,2nd,3rd,4th) 
+                  -> YYYY -> Year Full
+                */}
+                <p>
+                    {moment().format("dddd, MMMM D, YYYY")}
+                </p>
+
+            </div>
+
         </div>
     );
 };
